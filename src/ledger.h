dc5e4fa8b0234d944fd39d5e1d5f233bbd2f41c6
NyaHub <yaya70038@gmail.com>
2024-02-01 00:51:48 +0500

    Fix Ledger

--- src/ledger.h
+++ src/ledger.h
@@ -1,3 +1,5 @@
+#include <crypto++/config_int.h>
+#include <crypto++/hex.h>
 #include <crypto++/oids.h>
 #include <cryptopp/eccrypto.h>
 #include <cryptopp/filters.h>
@@ -10,33 +12,60 @@ namespace Mirucoin {
 class Ledger {
 private:
   CryptoPP::AutoSeededRandomPool prng;
-  CryptoPP::ECDSA<CryptoPP::ECP, CryptoPP::SHA256>::Signer signer;
+  CryptoPP::ECDSA<CryptoPP::ECP, CryptoPP::SHA256>::PrivateKey pk;
+  CryptoPP::ECDSA<CryptoPP::ECP, CryptoPP::SHA256>::PublicKey pubk;
 
 public:
-  Ledger() { signer.AccessKey().Initialize(prng, CryptoPP::ASN1::secp256k1()); }
+  Ledger() {
+    pk.Initialize(prng, CryptoPP::ASN1::secp256k1());
+    pk.MakePublicKey(pubk);
+  }
 
   std::string sign(std::string message) {
+    CryptoPP::ECDSA<CryptoPP::ECP, CryptoPP::SHA256>::Signer signer(pk);
+
     std::string signature;
 
     CryptoPP::StringSource s(
         message, true,
-        new CryptoPP::SignerFilter(prng, signer,
-                                   new CryptoPP::StringSink(signature)));
+        new CryptoPP::SignerFilter(
+            prng, signer,
+            new CryptoPP::HexEncoder(new CryptoPP::StringSink(signature))));
 
     return signature;
   }
 
-  auto verify(std::string message, std::string signature,
-              std::string derpublickey) {
-    CryptoPP::ECDSA<CryptoPP::ECP, CryptoPP::SHA256>::PublicKey publicKey;
+  std::string getDerPublic() {
+    std::string epubk;
+    CryptoPP::HexEncoder enc;
+    enc.Attach(new CryptoPP::StringSink(epubk));
+    pubk.Save(enc);
+    return epubk;
+  }
+  std::string getDerPrivate() {
+    std::string epk;
+    CryptoPP::HexEncoder enc;
+    enc.Attach(new CryptoPP::StringSink(epk));
+    pk.Save(enc);
+    return epk;
+  }
 
-    CryptoPP::StringSink sspublic(derpublickey);
+  auto verify(std::string message, std::string hexsignature,
+              std::string derpublickey) {
+    CryptoPP::HexDecoder dec;
+    dec.Put((CryptoPP::byte *)derpublickey.c_str(), derpublickey.size());
+    dec.MessageEnd();
 
     CryptoPP::ECDSA<CryptoPP::ECP, CryptoPP::SHA256>::Verifier verifier;
-    verifier.AccessKey().DEREncode(sspublic);
+    verifier.AccessKey().Load(dec);
     bool result = false;
 
-    CryptoPP::StringSource ss(
+    std::string signature;
+    CryptoPP::StringSource signss(
+        hexsignature, true,
+        new CryptoPP::HexDecoder(new CryptoPP::StringSink(signature)));
+
+    CryptoPP::StringSource outss(
         signature + message, true,
         new CryptoPP::SignatureVerificationFilter(
             verifier, new CryptoPP::ArraySink((CryptoPP::byte *)&result,
--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,32 @@
 #include "./blockchain.h"
+#include "./ledger.h"
 #include "./utils.h"
+#include <cstdio>
 #include <iostream>
 #include <ostream>
+#include <string>
 
 int main(int, char **) {
-  Mirucoin::Blockchain chain{"chain.sqlite"};
+  // Mirucoin::Blockchain chain{"chain.sqlite"};
 
-  Mirucoin::Block blk{chain.getlastHash(), chain.getLastIndex(), "first block"};
+  // Mirucoin::Block blk{chain.getlastHash(), chain.getLastIndex(), "first
+  // block"};
 
-  chain.addBlock(&blk);
+  // chain.addBlock(&blk);
 
-  chain.print();
+  // chain.print();
+
+  Mirucoin::Ledger wallet;
+
+  std::string message = "Some message";
+
+  printf("PrivateKey: %s\nPublicKey: %s\n", wallet.getDerPrivate().c_str(),
+         wallet.getDerPublic().c_str());
+
+  std::string sing = wallet.sign(message);
+
+  std::cout << sing << std::endl;
+  std::cout << wallet.verify(message, sing, wallet.getDerPublic()) << std::endl;
 
   return 0;
 }
